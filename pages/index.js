1154eb8bc5e6ef35b1f8145528845f72d828f44a
Richard <you@example.com>
2022-05-14 20:24:00 +0800

    [Add] random add

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Home() {
+    const closeApp = 
+    () =>{
+        cons
+    }
+
   return (
     <div className={styles.home}>
        <h2>Welcome to Gaming Vibes</h2>
       <div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id.</p>
       </div>
+      <h1>Random input</h1>
     </div>
   )
 }
